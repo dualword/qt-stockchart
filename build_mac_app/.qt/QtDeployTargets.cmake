8bc2fc90a5ccc53414e9a8063b763a481dbf9497
Dennis Lang <landenlabs2@gmail.com>
2026-04-01 22:00:12 -0400

    cleanup, rename stocks to qt-stockchart

--- build_mac_app/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_StockChart_FILE /Users/dennislang/opt/projects/qt-projects/stocks/build_app/StockChart.app/Contents/MacOS/StockChart)
-set(__QT_DEPLOY_TARGET_StockChart_TYPE EXECUTABLE)
